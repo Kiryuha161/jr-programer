fc306ea122801a045d98ca44a843aebc01ef7548
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2023-03-07 14:26:20 +0300

    Сохранение данных между сценами
    Сделал сохранение данных между сценами

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -13,12 +16,29 @@ public class MenuUIHandler : MonoBehaviour
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
+        MainManager.Instance.TeamColor = color;
     }
-    
+
     private void Start()
     {
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew(int scene)
+    {
+        SceneManager.LoadScene(scene); //1 - индекс сцены
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR   //# - условная компиляция, т.е. компиляция или пропуск части кода в зависимости от того существует ли условие или нет. Все строки с # не являются кодом. Они не будут 
+        //выполнены вообще, если не выполнено условие, его как будто не существует. Это инструкция для компилятора. Ещё это называют препроцессорами. Код 
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+   
     }
 }
